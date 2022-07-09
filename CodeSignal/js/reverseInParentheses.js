b677966deafbee79797e5d74cff9e01490f531b0
PiotrSierant <piotr.sierant96@gmail.com>
2022-07-09 16:54:19 +0200

    reverse In Parentheses

---
+++ CodeSignal/js/reverseInParentheses.js
@@ -0,0 +1,11 @@
+function solution(inputString) {
+    while(inputString.includes("(")) {
+        const findEnd = inputString.indexOf(")");
+        const findStart = inputString.lastIndexOf("(", findEnd);
+        const reverse = inputString.slice(findStart + 1, findEnd).split('').reverse().join('');
+        inputString = inputString.slice(0, findStart) + reverse + inputString.slice(findEnd + 1);
+    }
+    return inputString;
+}
+const str = '(bar)';
+console.log(solution(str));
