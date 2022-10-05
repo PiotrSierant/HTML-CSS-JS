b8a051d59622def5d2532d7ceabca9a046bd0f4d
PiotrSierant <piotr.sierant96@gmail.com>
2022-10-05 09:53:19 +0200

    ElectionsWinners

---
+++ CodeSignal/js/ElectionsWinners.js
@@ -0,0 +1,27 @@
+function solution(votes, k) {
+    const votesMax = Math.max(...votes);
+    const indexes = [];
+
+    for (let index = 0; index < votes.length; index++) {
+        if (votes[index] === votesMax) {
+            indexes.push(index);
+        }
+    }
+
+    if(k === 0 && indexes.length === 1) {
+        return 1
+    }
+
+    const raisedVotes = votes.map(element => {
+        return element + k;
+    })
+
+    const allTheVotesTheyCanWin = raisedVotes.filter(element => {
+        return element > votesMax;
+    })
+
+    return allTheVotesTheyCanWin.length;
+}
+const votes = [2, 3, 5, 2];
+const k = 3;
+console.log(solution(votes, k));
