44daa0b54dfa38da92d6d64b70d8ea7e729f3082
PiotrSierant <piotr.sierant96@gmail.com>
2022-10-17 10:16:57 +0200

    PagesNumberingWithInk

---
+++ CodeSignal/js/thecore/PagesNumberingWithInk.js
@@ -0,0 +1,27 @@
+function solution(current, numberOfDigits) {
+    while (numberOfDigits > 0) {
+        let n;
+        if (current >= 1000) {
+            n = Math.floor(numberOfDigits / 4);
+            numberOfDigits = 0;
+            current += n;
+        } else if (current >= 100) {
+            n = Math.min(1000 - current, Math.floor(numberOfDigits / 3))
+            current += n;
+            numberOfDigits -= n * 3;
+            if (numberOfDigits < 4)
+                numberOfDigits = 0
+        } else if (current >= 10) {
+            n = Math.min(100 - current, Math.floor(numberOfDigits / 2))
+            current += n;
+            numberOfDigits -= n * 2;
+            if (numberOfDigits < 3)
+                numberOfDigits = 0;
+        } else {
+            n = Math.min(10 - current, numberOfDigits)
+            current += n;
+            numberOfDigits -= n;
+        }
+    }
+    return current - 1;
+}
